e08d46a5ef1f0e3ef9510fd9cdd4860d9aef8d1a
xiaoqing.dongxq <xiaoqing.dongxq@alibaba-inc.com>
2014-08-25 12:04:56 +0800

    axis add

---
+++ README.md
@@ -0,0 +1,18 @@
+# achart-axis [![spm version](http://spmjs.io/badge/achart-axis)](http://spmjs.io/package/achart-axis)
+
+---
+
+
+
+## Install
+
+```
+$ spm install achart-axis --save
+```
+
+## Usage
+
+```js
+var achartAxis = require('achart-axis');
+// use achartAxis
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+
+var Axis = require('./src/base');
+  
+Axis.Category = require('./src/category');
+
+Axis.Number = require('./src/number');
+
+Axis.Time = require('./src/time');
+
+Axis.Auto = require('./src/auto');
+
+Axis.Circle = require('./src/circle');
+
+Axis.Radius = require('./src/radius');
+
+module.exports = Axis;
